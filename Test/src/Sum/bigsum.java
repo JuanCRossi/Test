f6c6198595f9b70bfcac51c7b43e79e3fa1630ed
Juan Rossi <juan.rossi@upr.edu>
2019-07-12 18:40:29 -0400

    Added basic sum.

---
+++ Test/src/Sum/bigsum.java
@@ -0,0 +1,15 @@
+package Sum;
+
+public class bigsum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 45;
+		int y = 75;
+		int total;
+		total = x + y;
+		System.out.println("The total is " + total);
+
+	}
+
+}
